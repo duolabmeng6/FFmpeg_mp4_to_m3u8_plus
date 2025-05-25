b2287ec09b09c2d365f2867cbe3fac05460036ad
多啦b梦 <1715109585@qq.com>
2025-05-25 18:02:35 +0800

    Update navigation.py

--- components/navigation.py
+++ components/navigation.py
@@ -3,6 +3,6 @@ import streamlit as st
 def show_navigation():
     with st.sidebar:
         st.title("🎯 功能导航")
-        st.page_link("Home.py", label="主页", icon="🏠")
+        st.page_link("main.py", label="主页", icon="🏠")
         st.page_link("pages/1_convert.py", label="视频转换", icon="🔄")
         st.page_link("pages/2_preview.py", label="视频预览", icon="📺") 
